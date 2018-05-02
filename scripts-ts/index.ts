9108d443796d80951a6968b634dde979d5ec1c92
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 00:21:26 +0900

    fix

--- scripts-ts/index.ts
+++ scripts-ts/index.ts
@@ -3,9 +3,6 @@ import hubot = require('hubot');
 module.exports = (robot: hubot.Robot<any>): void => {
 
   robot.hear(/^大将(！|!)$/, res => {
-    res.reply([
-      'ヘイ！お待ち！',
-      'http://www.tamasushi.co.jp/wordpress/wp-content/themes/xeory_base_child/lib/common/img/tabehoImg_02.jpg'
-    ].join("\n"));
+    res.reply('ヘイ！お待ち！');
   });
 };
