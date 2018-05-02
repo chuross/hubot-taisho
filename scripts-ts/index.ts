de65c80d8ebdf87716b6e53dc96fba97f4d10dd0
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 00:55:50 +0900

    add image

--- scripts-ts/index.ts
+++ scripts-ts/index.ts
@@ -1,8 +1,10 @@
 import hubot = require('hubot');
+import LineMessaging = require('hubot-line-messaging');
 
 module.exports = (robot: hubot.Robot<any>): void => {
 
   robot.hear(/^大将(！|!)$/, res => {
     res.reply('ヘイ！お待ち！');
+    res.reply(new LineMessaging.SendImage('https://www.instagram.com/p/BiQUYnElTyu/media?size=m', 'https://www.instagram.com/p/BiQUYnElTyu/media?size=l'))
   });
 };
