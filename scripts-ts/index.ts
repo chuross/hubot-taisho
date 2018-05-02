355014f54f8343f90dce601ba08ebf280c05db63
chuross <azarasi.mo.mo@gmail.com>
2018-05-02 22:13:29 +0900

    fix default script

--- scripts-ts/index.ts
+++ scripts-ts/index.ts
@@ -2,7 +2,7 @@ import hubot = require('hubot');
 
 module.exports = (robot: hubot.Robot<any>): void => {
 
-  robot.hear(/^大将！$/, res => {
+  robot.hear(/^大将$/, res => {
     res.send([
       'ヘイ！お待ち！',
       'http://www.tamasushi.co.jp/wordpress/wp-content/themes/xeory_base_child/lib/common/img/tabehoImg_02.jpg'
