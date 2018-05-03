7f7646227bae6530866b803e48cdb3edf54e9bfb
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 12:40:29 +0900

    add sushi

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,22 @@
 const LineMessaging = require('hubot-line-messaging');
 
+const sushiImageUrls = [
+  'https://www.instagram.com/p/BiOihl7lj6Z',
+  'https://www.instagram.com/p/BiQUYnElTyu',
+  'https://www.instagram.com/p/BiTHKhIFx3U',
+  'https://www.instagram.com/p/BiTEdbiloQ4',
+  'https://www.instagram.com/p/BiTCuIigAZ0',
+  'https://www.instagram.com/p/BiS_oRPn5_j',
+  'https://www.instagram.com/p/BiNQnaCFTHU',
+  'https://www.instagram.com/p/BiSzAkOHLOQ',
+  'https://www.instagram.com/p/BiSTvGgFjJR',
+  'https://www.instagram.com/p/BiSS5vBF93p'
+];
+
 module.exports = (robot) => {
   robot.hear(/^大将いつもの$/, res => {
-    const sushiBaseUrl = 'https://www.instagram.com/p/BiQUYnElTyu/media';
-    res.reply(new LineMessaging.SendImage(`${sushiBaseUrl}?size=l`, `${sushiBaseUrl}?size=l`));
+    const index = Math.floor(Math.random() * (sushiImageUrls.length - 0));
+    const sushiBaseUrl = sushiImageUrls[index];
+    res.reply(new LineMessaging.SendImage(`${sushiBaseUrl}/media?size=l`, `${sushiBaseUrl}/media?size=s`));
   });
 };
