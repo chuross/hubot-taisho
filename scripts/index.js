1ac76ceb601646de53213a19f03234fa1074807d
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 12:28:22 +0900

    リプライトークン一回きりっぽいので寿司画像だけにした

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,7 @@ const LineMessaging = require('hubot-line-messaging');
 
 module.exports = (robot) => {
   robot.hear(/^大将いつもの$/, res => {
-    res.reply('ヘイ！お待ち！');
-
     const sushiBaseUrl = 'https://www.instagram.com/p/BiQUYnElTyu/media';
-    setTimeout(() => {
-      res.reply(new LineMessaging.SendImage(`${sushiBaseUrl}?size=l`, `${sushiBaseUrl}?size=l`));
-    }, 100);
+    res.reply(new LineMessaging.SendImage(`${sushiBaseUrl}?size=l`, `${sushiBaseUrl}?size=l`));
   });
 };
