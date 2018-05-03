d8bafccafadba92f14b0e85112d3d0a4b0f73b2f
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 15:36:18 +0900

    fix url

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@ const LineMessaging = require('hubot-line-messaging');
 const cheerio = require('cheerio')
 const axios = require('axios');
 
-const weatherUrl = 'https://weather.yahoo.co.jp/weather/raincloud';
+const weatherUrl = 'https://weather.yahoo.co.jp/weather/raincloud/';
 
 module.exports = robot => {
   robot.hear(/^大将(!|！)アメッシュ$/, res => {
