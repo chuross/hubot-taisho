d32e64a22364a1427f3d3db48968bdf81c055bc4
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 14:13:31 +0900

    fix

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,7 +2,7 @@
 module.exports = robot => {
   robot.hear(/^大将(！|!)予定 (.+)/, res => {
     const adminUsers = (process.env.HUBOT_ADMIN || '').split(',');
-    if (!adminUsers.includes(res.user.id)) {
+    if (!adminUsers.includes(res.message.user.id)) {
       res.reply("お客さんそいつは聞けないねぇ\n権限振ってもらいな");
       return;
     }
