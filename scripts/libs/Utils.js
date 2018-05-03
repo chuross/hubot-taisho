ad9da58c42b11771647f97b9750346889229009f
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 18:49:12 +0900

    fix

--- scripts/libs/Utils.js
+++ scripts/libs/Utils.js
@@ -1,13 +1,19 @@
 
 module.exports = class Utils {
 
-  static adminUsers = (process.env.HUBOT_ADMIN || '').split(',');
+  static get adminUsers() {
+    return (process.env.HUBOT_ADMIN || '').split(',');
+  }
 
-  // Todoist
-  static todoistApiToken = process.env.HUBOT_TODOIST_TOKEN;
-  static todoistProjectId = process.env.HUBOT_TODOIST_PROJECT_ID
+  static get todoistApiToken() {
+    return process.env.HUBOT_TODOIST_TOKEN;
+  }
+
+  static get todoistProjectId() {
+    return process.env.HUBOT_TODOIST_PROJECT_ID;
+  }
 
   static get isLine() {
-    return process.env.ADAPTER_MODE == 'line'
+    return process.env.ADAPTER_MODE == 'line';
   }
 }
