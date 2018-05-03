14015546d02fb629baa75017653180dfc6520b89
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 11:46:01 +0900

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 const LineMessaging = require('hubot-line-messaging');
 
 module.exports = (robot) => {
-  robot.hear(/^大将(！|!)$/, res => {
+  robot.hear(/^大将いつもの$/, res => {
     res.reply('ヘイ！お待ち！');
-    res.reply('今日もいいの入ったよ')
 
     const sushiBaseUrl = 'https://www.instagram.com/p/BiQUYnElTyu/media';
     res.reply(new LineMessaging.SendImage(`${sushiBaseUrl}?size=l`, `${sushiBaseUrl}?size=l`));
