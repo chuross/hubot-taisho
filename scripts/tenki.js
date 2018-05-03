69f931122e5cd3a1034118c3e158f5e1ca7d7d1a
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 13:32:12 +0900

    add debugger

--- scripts/tenki.js
+++ scripts/tenki.js
@@ -4,6 +4,7 @@ const imageUrl = 'https://hailstorm.c.yimg.jp/iwiz-weather/raincloud/1525318200/
 
 module.exports = robot => {
   robot.hear(/^大将(!|！)アメッシュ$/, res => {
+    console.log(res);
     res.reply(new LineMessaging.SendImage(imageUrl, imageUrl));
   });
 };
