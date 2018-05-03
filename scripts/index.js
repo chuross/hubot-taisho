7396c509fa43b3815c5c1a0225c4ad31ece3141d
chuross <azarasi.mo.mo@gmail.com>
2018-05-03 23:45:26 +0900

    fix images

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,7 @@ const LineMessaging = require('hubot-line-messaging');
 const Utils = require('./libs/Utils');
 
 const sushiImageUrls = [
-  'https://www.instagram.com/p/BiOihl7lj6Z',
-  'https://www.instagram.com/p/BiQUYnElTyu',
-  'https://www.instagram.com/p/BiTHKhIFx3U',
-  'https://www.instagram.com/p/BiTEdbiloQ4',
-  'https://www.instagram.com/p/BiTCuIigAZ0',
-  'https://www.instagram.com/p/BiS_oRPn5_j',
-  'https://www.instagram.com/p/BiNQnaCFTHU',
-  'https://www.instagram.com/p/BiSzAkOHLOQ',
-  'https://www.instagram.com/p/BiSTvGgFjJR',
-  'https://www.instagram.com/p/BiSS5vBF93p',
-  'https://www.instagram.com/p/BiSKH1WFxmc'
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMcVb5qh8oOUNswJPB3XmGkmTs0odQ9AQ8P0ba-OuFVfbwEBFR'
 ];
 
 module.exports = robot => {
