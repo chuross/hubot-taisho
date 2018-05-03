608c78c9a93e21ca548c14807fa77a582f1ed137
chuross <azarasi.mo.mo@gmail.com>
2018-05-04 00:04:11 +0900

    add images

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,16 @@ const LineMessaging = require('hubot-line-messaging');
 const Utils = require('./libs/Utils');
 
 const sushiImageUrls = [
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb2E3lfTyr1WzbewSvt37rwe9yXjSDpQzdSxb3_iIl5aIGbSV19w'
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzX14FM3H-nja_M3w82EP28osjWfjtIQRa6QLLpWsECiGuxOlKg',
+  'https://tblg.k-img.com/restaurant/images/Rvw/59650/640x640_rect_59650578.jpg',
+  'https://d35omnrtvqomev.cloudfront.net/photo/article/article_part/image_path_1/25860/50f1bf46a8033d30770a31d94ce8e8.jpg',
+  'http://best-travel.tokyo/wp-content/uploads/2018/02/%E3%81%86%E3%81%AB_R.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu0owNvCKBw7K7sCuMrGcaP_l9tEpbkCSDSQIRYEmeJ3yuxQhrxQ',
+  'http://ninnpusan.com/img/uni.jpg',
+  'https://blog-imgs-70.fc2.com/i/c/h/ichii445/uni12.jpg',
+  'https://pu-3.com/manpuku/wp-content/uploads/20080815204849.jpg',
+  'https://tblg.k-img.com/restaurant/images/Rvw/68409/640x640_rect_68409433.jpg',
+  'https://favy-tokyo.s3.amazonaws.com/uploads/topic_item/image/364497/retina_IMG_1683.jpg'
 ];
 
 module.exports = robot => {
