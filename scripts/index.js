5cee3301adb928f494b05431caf38983e70bb110
chuross <azarasi.mo.mo@gmail.com>
2018-05-07 17:20:08 +0900

    add help command

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+// Commands:
+//   大将！いつもの - いつものあれ用意してますぜ
+
 const LineMessaging = require('hubot-line-messaging');
 const Utils = require('./libs/Utils');
 
@@ -16,12 +19,13 @@ const sushiImageUrls = [
 
 module.exports = robot => {
   robot.hear(/^大将(!|！)$/, res => {
-    res.reply([
-      '大将！いつもの - いつものあれ用意してますぜ',
-      '大将！アメッシュ - 関東地方の雨雲画像を表示しやすぜ',
-      "大将！予定 <日付> <タスク名> - 指定された日付でタスクをTodoistに追加しやすぜ\n(※要権限)\n  <日付> - 今日・明日・来週・来月またはmm-dd・yyyy-mm-dd",
-      "大将！献立 <キーワード>\n  <キーワード> - 材料や料理名など",
-    ].join("\n\n"));
+    res.reply(robot.helpCommands().join("\n\n"));
+    // res.reply([
+    //   '大将！いつもの - いつものあれ用意してますぜ',
+    //   '大将！アメッシュ - 関東地方の雨雲画像を表示しやすぜ',
+    //   "大将！予定 <日付> <タスク名> - 指定された日付でタスクをTodoistに追加しやすぜ\n(※要権限)\n  <日付> - 今日・明日・来週・来月またはmm-dd・yyyy-mm-dd",
+    //   "大将！献立 <キーワード>\n  <キーワード> - 材料や料理名など",
+    // ].join("\n\n"));
   });
 
   robot.hear(/^大将(!|！)いつもの$/, res => {
