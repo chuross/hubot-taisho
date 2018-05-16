9d15a95e603399db5bd79011c794d79b9b057f37
chuross <azarasi.mo.mo@gmail.com>
2018-05-16 19:48:25 +0900

    use async function wather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,19 +9,19 @@ const Utils = require('./libs/Utils');
 const weatherUrl = 'https://weather.yahoo.co.jp/weather/jp/raincloud/3.html';
 
 module.exports = robot => {
-  robot.hear(/^大将(!|！)アメッシュ$/, res => {
-    axios.get(`${weatherUrl}`, {
-      responseType: 'text'
-    })
-    .then(response => cheerio.load(response.data))
-    .then($ => $('#imgDatCh .mainImg img').attr("src"))
-    .then(imageUrl => {
+  robot.hear(/^大将(!|！)アメッシュ$/, async res => {
+    try {
+      const response = await axios.get(`${weatherUrl}`, { responseType: 'text' })
+      const $ = cheerio.load(response.data);
+      const imageUrl = $('#imgDatCh .mainImg img').attr("src");
+
       if (Utils.isLine) {
-        res.reply(new LineMessaging.SendImage(imageUrl, imageUrl))
+        res.reply(new LineMessaging.SendImage(imageUrl, imageUrl));
       } else {
-        res.reply(imageUrl)
+        res.reply(imageUrl);
       }
-    })
-    .catch(error => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
