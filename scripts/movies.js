3ded3e3ec5cc28f0edd2e9e806ff4f1967452089
chuross <azarasi.mo.mo@gmail.com>
2018-05-16 20:54:49 +0900

    use async function movie.js

--- scripts/movies.js
+++ scripts/movies.js
@@ -10,23 +10,21 @@ const baseUrl = 'https://filmarks.com';
 const rankUrl = `${baseUrl}/list/now`;
 
 module.exports = robot => {
-  robot.hear(/^大将(!|！)上映中の映画$/, res => {
-    axios.get(rankUrl, {
-      responseType: 'text'
-    })
-    .then(response => cheerio.load(response.data))
-    .then($ => $('.p-movies-grid .p-movie-cassette').map((index, node) => {
-      const $node = $(node);
-      return {
-        title: $node.find('.p-movie-cassette__info .p-movie-cassette__title').text(),
-        thumbnailUrl: $node.find('.p-movie-cassette__info .p-movie-cassette__jacket img').attr('src'),
-        linkUrl: `${baseUrl}${$node.find('.p-movie-cassette__info .p-movie-cassette__readmore').attr('href')}`,
-        startAt: $node.find('.p-movie-cassette__other-info span').eq(0).text(),
-        movieTime: $node.find('.p-movie-cassette__other-info span').eq(1).text()
-      };
-    }).get())
-    .then(result => result.slice(0, 10))
-    .then(result => {
+  robot.hear(/^大将(!|！)上映中の映画$/, async res => {
+    try {
+      const response = await axios.get(rankUrl, { responseType: 'text' });
+      const $ = cheerio.load(response.data);
+      const result = $('.p-movies-grid .p-movie-cassette').map((index, node) => {
+          const $node = $(node);
+          return {
+            title: $node.find('.p-movie-cassette__info .p-movie-cassette__title').text(),
+            thumbnailUrl: $node.find('.p-movie-cassette__info .p-movie-cassette__jacket img').attr('src'),
+            linkUrl: `${baseUrl}${$node.find('.p-movie-cassette__info .p-movie-cassette__readmore').attr('href')}`,
+            startAt: $node.find('.p-movie-cassette__other-info span').eq(0).text(),
+            movieTime: $node.find('.p-movie-cassette__other-info span').eq(1).text()
+          };
+        }).get().slice(0, 10);
+      
       if (!Utils.isLine) {
         res.reply('ヘイお待ち！上映中の映画だよ！', result.map(item => `${item.title}\n${item.linkUrl}`).join("\n\n"));
         return;
@@ -47,7 +45,8 @@ module.exports = robot => {
       });
       
       res.reply('ヘイお待ち！上映中の映画だよ！', messageBuilder.build());
-    })
-    .catch(error => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
